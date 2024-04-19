41c5ba599e4609f2fd70de37e92fc366fbf28193
ifahimkhan <khan.fahim95@hotmail.com>
2024-04-19 21:49:22 +0400

    initial commit of rabble wobble Swift iOS app following design pattern MVC

---
+++ RabbleWabble/Models/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by Fahim on 4/18/24.
+//
+
+import Foundation
+
+public struct Question{
+    public let answer: String
+    public let hint:String?
+    public let prompt:String
+}
---
+++ RabbleWabble/Models/QuestionGroup.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabble
+//
+//  Created by Ragesh on 4/18/24.
+//
+
+import Foundation
+public struct QuestionGroup{
+    public let questions:[Question]
+    public let title:String
+}
